0f3492b8dc56bdd75e2e129a20408041fd55e85b
dimonvideo-beep <dimonvideo@gmail.com>
2020-07-31 09:05:23 +0300

    1.7 is_more

--- app/src/main/java/com/dimonvideo/client/ui/main/MainFragment.java
+++ app/src/main/java/com/dimonvideo/client/ui/main/MainFragment.java
@@ -52,10 +52,11 @@ public class MainFragment extends Fragment  {
         TabLayout tabLayout = root.findViewById(R.id.tabLayout);
         ViewPager viewPager = root.findViewById(R.id.view_pager);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(requireContext());
+        final boolean is_more = sharedPrefs.getBoolean("dvc_more", false);
         String login = sharedPrefs.getString("dvc_password", "");
         TabsAdapter adapt = new TabsAdapter(getChildFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, getContext());
         adapt.addfrg(new MainFragmentContent(),getString(R.string.tab_last));
-        adapt.addfrg(new MainFragmentHorizontal(),getString(R.string.tab_details));
+        if (is_more) adapt.addfrg(new MainFragmentHorizontal(),getString(R.string.tab_details));
         adapt.addfrg(new MainFragmentCats(),getString(R.string.tab_categories));
         if ((login != null) && (login.length() > 2)) adapt.addfrg(new MainFragmentFav(),getString(R.string.tab_favorites));
 
