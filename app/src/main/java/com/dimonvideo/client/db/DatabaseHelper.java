5cf6e3d4df2ae6d4bcf4d8f976a790afa9bb1036
admin <dimon@dimonvideo.ru>
2024-09-03 16:27:39 +0300

    - добавлена возможность сохранения - восстановления настроек (после авторизации)  - оптимизация работы

--- app/src/main/java/com/dimonvideo/client/db/DatabaseHelper.java
+++ app/src/main/java/com/dimonvideo/client/db/DatabaseHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "dvclient";
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
